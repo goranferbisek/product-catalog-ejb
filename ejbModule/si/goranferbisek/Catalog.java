4e8b1b3693e6d2b8d62a27ced51c52d2fe3fbbc2
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-03-10 17:12:38 +0100

    Make a EJB project

---
+++ ejbModule/si/goranferbisek/Catalog.java
@@ -0,0 +1,31 @@
+package si.goranferbisek;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Catalog
+ */
+@Stateless
+@LocalBean
+public class Catalog implements CatalogLocal {
+
+	private List<CatalogItem> items = new ArrayList<>();
+    
+    public Catalog() {
+    }
+
+	@Override
+	public List<CatalogItem> getItems() {
+		return this.items;
+	}
+
+	@Override
+	public void addItem(CatalogItem item) {
+		this.items.add(item);		
+	}
+
+}
---
+++ ejbModule/si/goranferbisek/CatalogLocal.java
@@ -0,0 +1,13 @@
+package si.goranferbisek;
+
+import java.util.List;
+
+import javax.ejb.Local;
+
+@Local
+public interface CatalogLocal {
+
+	public List<CatalogItem> getItems();
+	
+	public void addItem(CatalogItem item);
+}
