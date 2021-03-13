87329c8d7e4f8c6b6dc954a8ff30f9db34efc13c
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-03-13 19:37:07 +0100

    Persist to database instead saving to a list

--- ejbModule/si/goranferbisek/Catalog.java
+++ ejbModule/si/goranferbisek/Catalog.java
@@ -1,10 +1,11 @@
 package si.goranferbisek;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 /**
  * Session Bean implementation class Catalog
@@ -13,19 +14,20 @@ import javax.ejb.Singleton;
 @LocalBean
 public class Catalog implements CatalogLocal {
 
-	private List<CatalogItem> items = new ArrayList<>();
+	@PersistenceContext
+	private EntityManager entityManager;
     
     public Catalog() {
     }
 
 	@Override
 	public List<CatalogItem> getItems() {
-		return this.items;
+		return this.entityManager.createQuery("select c from CatalogItem c", CatalogItem.class).getResultList();
 	}
 
 	@Override
 	public void addItem(CatalogItem item) {
-		this.items.add(item);		
+		this.entityManager.persist(item);		
 	}
 
 }
