3be61fa2cf93346a87d9c0090e01bb5c1f8b3e35
Goran Ferbišek <goran.ferbisek@gmail.com>
2021-03-23 18:29:53 +0100

    Add a custom validator

--- ejbModule/si/goranferbisek/CatalogItem.java
+++ ejbModule/si/goranferbisek/CatalogItem.java
@@ -39,7 +39,7 @@ public class CatalogItem {
 	@Column(name="NAME")
 	private String name;
 	
-	@Pattern(regexp = "^[A-Za-z]*$", message = "Must be letters")
+	@Alphabetic
 	@Column(name="MANUFACTURER")
 	private String manufacturer;
 	
